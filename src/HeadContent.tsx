79c79f1da8e1f6a9f91863249702de3267167172
Juyeong Maing <mjy9088@naver.com>
2024-06-11 20:52:48 +0900

    fix katex style by adding HeadContent

---
+++ src/HeadContent.tsx
@@ -0,0 +1,10 @@
+export function HeadContent() {
+  return (
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/katex.css"
+      integrity="sha384-K1E1xaIzoWihlXfiT5fcmLNabsnrl+dqc0errnRwtMX14tKRA9cCYNDGnXiHEZMs"
+      crossOrigin="anonymous"
+    />
+  );
+}
