f8933f0d15b20ddb5a0d6380eb9cbf348186647f
Juyeong Maing <mjy9088@naver.com>
2024-06-17 19:37:08 +0900

    refactor hsvToRgb

--- src/util/hsvToRgb.ts
+++ src/util/hsvToRgb.ts
@@ -7,47 +7,31 @@ export function hsvToRgb(
   let g = 0;
   let b = 0;
 
-  if (s === 0) {
-    r = g = b = v;
-  } else {
-    let type = Math.floor(h * 6);
-    let progress = h * 6 - type;
-    let p = v * (1 - s);
-    let q = v * (1 - progress * s);
-    let t = v * (1 - (1 - progress) * s);
+  let type = Math.floor(h * 6);
+  let progress = h * 6 - type;
+  let p = v * (1 - s);
+  let q = v * (1 - progress * s);
+  let t = v * (1 - (1 - progress) * s);
 
-    switch (type % 6) {
-      case 0:
-        r = v;
-        g = t;
-        b = p;
-        break;
-      case 1:
-        r = q;
-        g = v;
-        b = p;
-        break;
-      case 2:
-        r = p;
-        g = v;
-        b = t;
-        break;
-      case 3:
-        r = p;
-        g = q;
-        b = v;
-        break;
-      case 4:
-        r = t;
-        g = p;
-        b = v;
-        break;
-      case 5:
-        r = v;
-        g = p;
-        b = q;
-        break;
-    }
+  switch (type % 6) {
+    case 0:
+      [r, g, b] = [v, t, q];
+      break;
+    case 1:
+      [r, g, b] = [q, v, p];
+      break;
+    case 2:
+      [r, g, b] = [q, v, t];
+      break;
+    case 3:
+      [r, g, b] = [p, q, v];
+      break;
+    case 4:
+      [r, g, b] = [t, p, v];
+      break;
+    case 5:
+      [r, g, b] = [v, p, q];
+      break;
   }
 
   return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
