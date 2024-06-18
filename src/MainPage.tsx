2fb8f57c789b2d6b34fa2a9394dc3c631646d844
Juyeong Maing <mjy9088@naver.com>
2024-06-18 23:34:00 +0900

    change MainPage to hierarchy

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,3 +1,11 @@
+import { HierarchyTop } from './components/hierarchy/HierarchyTop';
+
 export function MainPage() {
-  return <div>Under construction ...</div>;
+  return (
+    <div className="relative flex mx-auto w-full box-border p-[24px] max-w-[1600px]">
+      <div className="relative hidden desktop:block w-[25%]" />
+      <HierarchyTop />
+      <div className="hidden tablet:block tablet:w-[30%] desktop:w-[25%]" />
+    </div>
+  );
 }
